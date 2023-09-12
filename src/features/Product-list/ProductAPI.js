6d807c952d539574e1d5f45e872d6aa28464a814
mayankkansal1 <mayankkansal1@gmail.com>
2023-09-12 16:23:01 +0530

    Part 7 : filter and sort changes

--- src/features/Product-list/ProductAPI.js
+++ src/features/Product-list/ProductAPI.js
@@ -9,12 +9,23 @@ export function fetchAllProducts() {
     );
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter, sort) {
+    // filter = {"category":["smartphone","laptops"]}
+    // sort = {_sort:"price",_order="desc"}
+
+    // TODO : on server we will support multi values in filter
     let queryString = ''
     for (let key in filter) {
-        queryString += `${key}=${filter[key]}&`
+        const categoryValues = filter[key]
+        if (categoryValues.length) {
+            const lastCategoryValue =
+                categoryValues[categoryValues.length - 1]
+            queryString += `${key}=${lastCategoryValue}&`
+        }
     }
-
+    for (let key in sort) {
+        queryString += `${key}=${sort[key]}&`
+    }
     return new Promise(async (resolve) => {
         const response = await fetch('http://localhost:8080/products?' + queryString)
         const data = await response.json()
